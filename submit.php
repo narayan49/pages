9b90a26b5c7b64e1c684664e5669909c9e29a56f
Narayan Singh <68994093+narayan49@users.noreply.github.com>
2023-10-23 08:50:41 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,28 @@
+<?php
+$db_hostname="127.0.0.1";
+$db_username="root";
+$db_password="";
+$db_name="test";
+
+$conn=mysqli_connect($db_hostname,$db_username,$db_password,$db_name);
+
+if(!$conn){
+    echo "error".mysqli_connect_error();
+    exit ;
+}
+$name=$_POST['name'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$sql="INSERT INTO users(name,email,password) VALUES('$name','$email','$password')";
+$result=mysqli_query($conn,$sql);
+if(!$result){
+    echo"error".mysqli_error($conn);
+    exit;
+}
+echo "Registration Successful!";
+mysqli_close($conn);
+
+
+
+?>
